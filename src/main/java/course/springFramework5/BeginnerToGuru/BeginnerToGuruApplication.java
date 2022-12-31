bde15815e176109ce054ed2b1fc6a25ce112aeed
Marcin <01168939@pw.edu.pl>
2022-12-31 23:27:27 +0100

    Initial commit

---
+++ src/main/java/course/springFramework5/BeginnerToGuru/BeginnerToGuruApplication.java
@@ -0,0 +1,13 @@
+package course.springFramework5.BeginnerToGuru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeginnerToGuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeginnerToGuruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/course/springFramework5/BeginnerToGuru/BeginnerToGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package course.springFramework5.BeginnerToGuru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeginnerToGuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
